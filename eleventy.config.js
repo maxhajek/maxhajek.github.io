33a478c2ee19f5896b7324cb8350b30107cc0931
Maximilian Hajek <maxhajek@outlook.de>
2024-01-26 11:48:46 +0100

    Add favicon passthrough copy and update base.njk with favicon links

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy("src/favicon");
   return {
     dir: {
       input: "src",
